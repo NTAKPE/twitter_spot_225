ea7884c33ae146cc2b60c9e9b38fed995df9bf1d
NTAKPE <jm.ntakpe@gmail.com>
2020-05-01 11:44:47 +0000

    Remove reply to keywords

--- autoreply.py
+++ autoreply.py
@@ -14,16 +14,8 @@ def check_mentions(api, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
 
-    keywords = ['proverbe', 'dicton']
-
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id = since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-
-        #Reply to any tweet reply or not as long as it contained the keyword
-        if any(keyword in tweet.text.lower() for keyword in keywords):
-            if not tweet.user.following:
-                tweet.user.follow()
-            api.update_status(status= f"Hey {tweet.user.name}\n{gen_proverb()}", in_reply_to_status_id=tweet.id)
         
         #Reply only to tweet that is not a reply
         if tweet.in_reply_to_status_id is not None:
@@ -31,7 +23,7 @@ def check_mentions(api, since_id):
 
         if not tweet.user.following:
             tweet.user.follow()
-        api.update_status(status= f"Hey {tweet.user.name}\n{gen_proverb()}", in_reply_to_status_id=tweet.id)
+        api.update_status(status= f"Hey @{tweet.user.name}\n{gen_proverb()}", in_reply_to_status_id=tweet.id)
     return new_since_id
 
 def main():
