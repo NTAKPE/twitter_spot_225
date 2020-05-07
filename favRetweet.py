2141ebe53a9e226c39c6ee94c2fb120db45beb84
NTAKPE <jm.ntakpe@gmail.com>
2020-05-07 07:03:49 +0000

    Add #kebetu tag

--- favRetweet.py
+++ favRetweet.py
@@ -48,4 +48,4 @@ def main(keywords):
     time.sleep(INTERVAL)
 
 if __name__ == "__main__":
-    main(["#CIV225","#amoulanfe" ,"#covid19CIV", "#wasexo", "#opinion_citoyenne"])
+    main(["#CIV225","#amoulanfe", "#kebetu","#covid19CIV", "#wasexo", "#opinion_citoyenne"])
