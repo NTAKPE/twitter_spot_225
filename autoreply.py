3cdd28b9ed4e345d9cd0b9ffd097614f3e3a80fa
NTAKPE <jm.ntakpe@gmail.com>
2020-05-08 16:26:33 +0000

    Reconfigure autoreply bot

--- autoreply.py
+++ autoreply.py
@@ -16,16 +16,20 @@ def check_mentions(api, since_id):
 
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id = since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        
+
+        if tweet.in_reply_to_status_id is not None:
+            continue
+
         if not tweet.user.following:
             tweet.user.follow()
+            
         api.update_status(status= f" {gen_proverb()}", in_reply_to_status_id = tweet.id,\
             auto_populate_reply_metadata=True)
     return new_since_id
 
 def main():
     api = create_api()
-    since_id = 1
+    since_id = 1258698161281019910
     while True:
         since_id = check_mentions(api, since_id)
         logger.info("Waiting...")
--- generate_proverbs.py
+++ generate_proverbs.py
@@ -2,12 +2,12 @@
 # /Users/jeremientakpe/TwitterSpotBot/generate_proverbs.py
 
 import random
-from data import subs, chin, fran, kaby
+from data import subs, chin, fran, kaby, arab, asie
 
 def getfamily():
     #Get proverbs family randomly
     prov_family = [("Proverbe subsaharien", subs), ("Proverbe chinois", chin), ("Proverbe français", fran),\
-         ("Proverbe kabyle", kaby)]
+         ("Proverbe kabyle", kaby), ("Proverbe arabe", arab), ("Proverbe d'Asie", asie)]
     
     return random.choice(prov_family)
 
