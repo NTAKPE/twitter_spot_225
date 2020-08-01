ee131250e3386edd73ce97547410ecd69a193283
NTAKPE <jm.ntakpe@gmail.com>
2020-08-01 10:22:39 +0000

    Drogba added

--- favRetweet.py
+++ favRetweet.py
@@ -48,4 +48,4 @@ def main(keywords):
     time.sleep(INTERVAL)
 
 if __name__ == "__main__":
-    main(["#CIV225","#amoulanfe", "#civelections2020","#Drogba", "#wasexo", "#rapivoire", "rap ivoire", "rap kmer"])
+    main(["#CIV225","#amoulanfe", "#civelections2020","#Drogba", "Drogba", "#wasexo", "#rapivoire", "rap ivoire", "rap kmer"])
