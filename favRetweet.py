2a9c8e37e5918e3f53a4c5776577c2d80c538dc0
JN <49768086+NTAKPE@users.noreply.github.com>
2022-10-01 16:53:08 +0000

    Update favRetweet.py

--- favRetweet.py
+++ favRetweet.py
@@ -48,4 +48,4 @@ def main(keywords):
     time.sleep(INTERVAL)
 
 if __name__ == "__main__":
-    main(["#CIV225","#statsciv", "statsciv"])
+    main(["#CIV225","#junteFrancaiseDehors", "#BurkinaFasoLibre"])
